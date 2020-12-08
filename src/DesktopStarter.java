1b3f08e3669212b85c30b30eb4d2e14eb83c7fd6
Jordan Ítalo Amaral <jordanitalovicente@gmail.com>
2020-12-08 15:02:44 -0300

    update commit
    estabelecendo configurações básicas para o jogo donkey kong

---
+++ src/DesktopStarter.java
@@ -0,0 +1,10 @@
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class DesktopStarter{
+
+	public static void main (String[] args) {
+	      System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+
+        new LwjglApplication(new SimpleGame(), "Tutorial06", 800, 600, false);
+	}
+}
