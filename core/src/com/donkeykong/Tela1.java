a985c364fc8168179054af8b2281fa43f8dc881e
JordanAmaralVicente <jordanitalovicente@gmail.com>
2021-01-21 12:32:54 -0300

    Projeto Base Part III (Base finilizada)

--- core/src/com/donkeykong/Tela1.java
+++ core/src/com/donkeykong/Tela1.java
@@ -32,7 +32,7 @@ public class Tela1 extends ScreenAdapter {
         {//Início das configurações da janela do Jogo
             Gdx.gl.glClearColor(0, 1, 1, 1);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-            Gdx.graphics.setTitle("Donkey Kong - Tela inicial");
+            Gdx.graphics.setTitle("Tela 1");
             //Tamanho da janela: 640x480
 
         }//Fim das configurações de jane
@@ -47,7 +47,9 @@ public class Tela1 extends ScreenAdapter {
 
         if (Gdx.input.isTouched()) { // Verifica se o Botão foi clicado através do mouse
             System.out.println("Input X: " + Gdx.input.getX() + "\n Input Y: " + Gdx.input.getY());
-            game.btnStart.checkIfClicked(Gdx.input.getX(), Gdx.input.getY());
+            if(game.btnStart.checkIfClicked(Gdx.input.getX(), Gdx.input.getY())){
+                game.setScreen(new Tela2(game));
+            }
         }
     }//Fim das verificações de input
 
--- core/src/com/donkeykong/Tela2.java
+++ core/src/com/donkeykong/Tela2.java
@@ -13,15 +13,16 @@ public class Tela2 extends ScreenAdapter {
         this.game = game;
     }
 
-
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, .25f, 0, 1);
+        Gdx.gl.glClearColor(10, .25f, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.graphics.setTitle("Tela 2");
+
         game.batch.begin();
-        game.font.draw(game.batch, "Leroy", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
-        game.font.draw(game.batch, "Leroi.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
-        game.font.draw(game.batch, "Lerooi", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
+            game.font.draw(game.batch, "Leroy", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
+            game.font.draw(game.batch, "Leroi.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
+            game.font.draw(game.batch, "Lerooi", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
         game.batch.end();
     }
 }
